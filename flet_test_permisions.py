d82a4aac103c0648492d17d1bfb4b18f0e42746a
H <hos@hos.com>
2025-10-07 13:19:30 +0300

    initial commit with permission test code

---
+++ flet_test_permisions.py
@@ -0,0 +1,44 @@
+import flet as ft
+from flet.core.control_event import ControlEvent
+
+def main(page : ft.Page):
+    page.scroll = ft.ScrollMode.ADAPTIVE
+    page.appbar = ft.AppBar(title=ft.Text("اختبار صلاحيات"))
+    ph = ft.PermissionHandler()
+    page.overlay.append(ph)
+
+    def check_permission(e : ControlEvent) :
+        
+        print("Type E is : ",dir(e))
+        print("Data from E is : ",e.control.data)
+        o = ph.check_permission(e.control.data)
+        print("O is After check permission : ", o)
+        page.add(ft.Text(f"Checked {e.control.data.name}: {o}"))
+
+    def request_permission(e: ControlEvent):
+        o = ph.request_permission(e.control.data)
+        page.add(ft.Text(f"Requested {e.control.data.name}: {o}"))
+        print("O is After request permission : ", o)
+    
+    def open_app_settings(e: ControlEvent):
+        o = ph.open_app_settings()
+        page.add(ft.Text(f"App Settings: {o}"))
+        print("O is After open settings : ", o)
+
+    page.add(
+        ft.OutlinedButton(
+            "اختبار صلاحيه جهات الاتصال",
+            data=ft.PermissionType.CONTACTS,
+            on_click=check_permission
+        ),
+        ft.OutlinedButton(
+            "ارسال طلب الى صلاحيه جهات الاتصال",
+            data = ft.PermissionType.CONTACTS,
+            on_click=request_permission
+        ),
+        ft.OutlinedButton(
+            "Open App Settings",
+            on_click=open_app_settings
+        )
+    )
+ft.app(main)
