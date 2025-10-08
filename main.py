edc4698b69e53547ca289bb4dbb507684a39e731
H <hos@hos.com>
2025-10-08 14:25:01 +0300

    install flet 0.23.0

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import flet as ft
 
+# from flet.core.control_event import ControlEvent
 
-def main(page : ft.Page):
+
+def main(page: ft.Page):
     page.scroll = ft.ScrollMode.ADAPTIVE
     page.appbar = ft.AppBar(title=ft.Text("اختبار صلاحيات"))
     ph = ft.PermissionHandler()
     page.overlay.append(ph)
 
-    def check_permission(e ) :
-        
-        print("Type E is : ",dir(e))
-        print("Data from E is : ",e.control.data)
+    def check_permission(e):
+
+        print("Type E is : ", dir(e))
+        print("Data from E is : ", e.control.data)
         o = ph.check_permission(e.control.data)
         print("O is After check permission : ", o)
         page.add(ft.Text(f"Checked {e.control.data.name}: {o}"))
@@ -19,7 +21,7 @@ def main(page : ft.Page):
         o = ph.request_permission(e.control.data)
         page.add(ft.Text(f"Requested {e.control.data.name}: {o}"))
         print("O is After request permission : ", o)
-    
+
     def open_app_settings(e):
         o = ph.open_app_settings()
         page.add(ft.Text(f"App Settings: {o}"))
@@ -29,16 +31,15 @@ def main(page : ft.Page):
         ft.OutlinedButton(
             "اختبار صلاحيه جهات الاتصال",
             data=ft.PermissionType.CONTACTS,
-            on_click=check_permission
+            on_click=check_permission,
         ),
         ft.OutlinedButton(
             "ارسال طلب الى صلاحيه جهات الاتصال",
-            data = ft.PermissionType.CONTACTS,
-            on_click=request_permission
+            data=ft.PermissionType.CONTACTS,
+            on_click=request_permission,
         ),
-        ft.OutlinedButton(
-            "Open App Settings",
-            on_click=open_app_settings
-        )
+        ft.OutlinedButton("Open App Settings", on_click=open_app_settings),
     )
+
+
 ft.app(main)
